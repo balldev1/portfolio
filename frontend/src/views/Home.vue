263c8f497b0b4c20adcc1237b642adbfbc6e6096
balldev1 <balldev1@hotmail.com>
2025-04-26 10:38:49 +0700

    png

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="w-full flex gap-5 flex-col items-center justify-center pt-5">
-    <img src="/project/nanthawat.webp" />
+    <img src="/project/nanthawat.png" />
   </div>
 </template>
