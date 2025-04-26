a9992da4d8cef74fd29a4892364e97e4b1565832
balldev1 <balldev1@hotmail.com>
2025-04-26 10:46:38 +0700

    resume.png

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="w-full flex gap-5 flex-col items-center justify-center pt-5">
-    <img src="/project/nanthawat.png" />
+    <img src="/project/dev1.png" />
   </div>
 </template>
