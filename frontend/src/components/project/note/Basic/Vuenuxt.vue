69e74212210b3152bf8e91a5404977d1a2f78a3e
balldev1 <balldev1@hotmail.com>
2025-05-06 16:12:16 +0700

    vuenuxt

--- frontend/src/components/project/note/Basic/Vuenuxt.vue
+++ frontend/src/components/project/note/Basic/Vuenuxt.vue
@@ -19,7 +19,7 @@
     </span>
 
     <div class="mt-5 flex flex-col gap-5 w-full items-center">
-      <img src="/project/vue.jpeg" loading="lazy" class="w-[40rem]" />
+      <img src="/project/vuenuxt.png" loading="lazy" class="w-[40rem]" />
     </div>
   </div>
 </template>
