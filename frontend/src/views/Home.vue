96a0c209346dd4121e8b906be082d292af724786
balldev1 <balldev1@hotmail.com>
2025-05-06 14:37:58 +0700

    home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+import Basic from "./Basic.vue";
+</script>
+
 <template>
   <div class="w-full flex gap-5 flex-col items-center justify-center pt-5">
     <!-- <img src="/project/dev1.png" /> -->
-    Coming Soon
+    <Basic />
   </div>
 </template>
