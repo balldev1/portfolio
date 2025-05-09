89d98bb89211cf5b3682868f121b6058f66e4c23
balldev1 <balldev1@hotmail.com>
2025-05-09 09:37:22 +0700

    pub_sub

--- frontend/src/components/project/note/Pub_sub.vue
+++ frontend/src/components/project/note/Pub_sub.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex flex-col gap-4 w-full">
-    <h1 class="text-4xl underline underline-offset-8 font-semibold">Pub_sub</h1>
+    <h1 class="text-4xl underline underline-offset-8 font-semibold">
+      Publish (topic) / Subscribe (sub)
+    </h1>
 
     <span
       class="text-gray-300 text-lg text-balance mt-2 badge p-4 shadow shadow-gray-500"
